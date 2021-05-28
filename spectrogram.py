01bd0a24d5a614baf2a8d0b6b307e6213497882b
anbreton <58612089+anbreton@users.noreply.github.com>
2021-05-27 22:18:00 -0700

    added configfile with path
    it works! Except the sound doesn't play until you exit the graph. Need to adjust the graph spacing cause it's all squished right now.

---
+++ spectrogram.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 27 09:45:09 2021
+
+@author: amandabreton
+"""
+# %%
+#import the pyplot and wavfile modules 
+import matplotlib.pyplot as plot
+from scipy.io import wavfile
+from playsound import playsound
+import argparse 
+import yaml 
+import os 
+import random
+
+parser = argparse.ArgumentParser()
+parser.add_argument('config_filename')
+args = parser.parse_args()
+CONFIG_FILE = args.config_filename
+
+with open(CONFIG_FILE) as f:
+    configs = yaml.load(f, Loader=yaml.SafeLoader)
+    
+path = configs['path']
+files = os.listdir(path)
+sound = os.path.join(path, random.choice(files))
+
+# Read the wav file (mono)
+samplingFrequency, signalData = wavfile.read(sound)
+
+# Plot the signal read from wav file
+plot.subplot(211)
+plot.title('Recorded Sound')
+
+plot.plot(signalData)
+#plot.xlabel('Sample')
+plot.ylabel('Amplitude')
+
+plot.subplot(212)
+plot.title('Spectrogram')
+plot.specgram(signalData,Fs=samplingFrequency)
+plot.xlabel('Time')
+plot.ylabel('Frequency')
+
+plot.show()
+playsound(sound)
+
