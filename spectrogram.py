bc627641cef06a92bafabc61055762ff475ca70f
anbreton <58612089+anbreton@users.noreply.github.com>
2021-06-01 18:03:48 -0700

    uploading for consistency
    nothing is different i don't think, I just did a whole bunch of troubleshooting with the xenocanto mp3 files 
    apparently spectrogram python can't do mp3 files 
    i converted the mp3 file to a wav file and it doesn't work it just causes a bunch of issues with python and spyder

--- spectrogram.py
+++ spectrogram.py
@@ -5,7 +5,7 @@ Created on Thu May 27 09:45:09 2021
 
 @author: amandabreton
 """
-# %%
+# 
 #import the pyplot and wavfile modules 
 import matplotlib.pyplot as plot
 from scipy.io import wavfile
@@ -17,15 +17,16 @@ import random
 import wave
 import contextlib
 
-parser = argparse.ArgumentParser()
-parser.add_argument('config_filename')
-args = parser.parse_args()
-CONFIG_FILE = args.config_filename
+#parser = argparse.ArgumentParser()
+#parser.add_argument('config_filename')
+#args = parser.parse_args()
+#CONFIG_FILE = args.config_filename
 
-with open(CONFIG_FILE) as f:
-    configs = yaml.load(f, Loader=yaml.SafeLoader)
+#with open(CONFIG_FILE) as f:
+#    configs = yaml.load(f, Loader=yaml.SafeLoader)
     
-path = configs['path']
+#path = configs['path']
+path = '/Users/amandabreton/Documents/GitHub/gnatcatcher/sounds'
 files = os.listdir(path)
 sound = os.path.join(path, random.choice(files))
 
