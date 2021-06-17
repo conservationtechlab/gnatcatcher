86e9841b0f3bf6b8932e359f866fdcc63823a8c3
anbreton <58612089+anbreton@users.noreply.github.com>
2021-06-16 23:26:09 -0700

    cleaned up code, need to add argparse

--- sortaudiomoth.py
+++ sortaudiomoth.py
@@ -5,55 +5,55 @@ Created on Tue Jun 15 09:13:31 2021
 
 @author: amandabreton
 """
-import os 
-import random 
+import os
+import random
 import csv
 import pandas
 import wave
 import matplotlib.pyplot as plt
 import scipy
-from scipy.io.wavfile import write,read
 import contextlib
-import numpy as np 
-import math 
+import numpy as np
+import math
 
 csvpath = "/Users/amandabreton/Documents/GitHub/gnatcatcher/"
 path = '/Users/amandabreton/Documents/GitHub/gnatcatcher/sounds'
 files = os.listdir(path)
 sound = os.path.join(path, random.choice(files))
 
-#%% extracting data in a simpler but longer way 
+# %%
+# extracting data in a simpler but longer way
 
 if os.path.exists(os.path.join(csvpath, "audiocsv.csv")):
-  os.remove(os.path.join(csvpath, "audiocsv.csv"))
+    os.remove(os.path.join(csvpath, "audiocsv.csv"))
 else:
-  #print("no audiocsv.csv file")
-  pass
+    # print("no audiocsv.csv file")
+    pass
 
 nonimagecount = 0
 if os.path.exists(os.path.join(path, ".DS_Store")):
-  os.remove(os.path.join(path, ".DS_Store"))
+    os.remove(os.path.join(path, ".DS_Store"))
 else:
-  #print("no .DS_Store files")
-  pass 
-  
+    # print("no .DS_Store files")
+    pass
+
 for filename in os.listdir(path):
     if filename.endswith(".wav"):
         name = os.path.join(path, filename)
     else:
         nonimagecount = +1
-        continue  
-   
-sounds = [os.path.join(path, name) for name in os.listdir(path) if 
-        os.path.isfile(os.path.join(path, name))]
+        continue
+
+sounds = [os.path.join(path, name) for name in os.listdir(path) if
+          os.path.isfile(os.path.join(path, name))]
 
 length = len(sounds)
 entrynum = list(range(0, length))
-    
+
 channelarray = []
 samplerates = []
 framenums = []
-durationarray= []
+durationarray = []
 IFarray = []
 loudness = []
 loudtime = []
@@ -68,9 +68,9 @@ for k in range(length):
     samplerates.append(fs)
     frames = file.getnframes()
     framenums.append(frames)
-    fs,audData=scipy.io.wavfile.read(sample)
+    fs, audData = scipy.io.wavfile.read(sample)
     Pxx, freqs, bins, im = plt.specgram(audData, Fs=fs, NFFT=1024)
-    with contextlib.closing(wave.open(sample,'r')) as f:
+    with contextlib.closing(wave.open(sample, 'r')) as f:
         frames = f.getnframes()
         rate = f.getframerate()
         duration = frames / float(rate)
@@ -82,16 +82,16 @@ for k in range(length):
         freqsnew = np.delete(freqs, (0), axis=0)
         row, col = np.where(Pxx == np.max(Pxx))
         intensF = freqsnew[row][0]
-    IFarray.append(intensF)   
-    dec = math.log(np.max(Pxx))  
-    #double check the math for calculating decibels 
+    IFarray.append(intensF)
+    dec = math.log(np.max(Pxx))
+    # double check the math for calculating decibels
     loudness.append(dec)
     x = ((col*duration)/len(bins))[0]
     loudtime.append(x)
     avgL = math.log(np.mean(Pxx))
     avgloudness.append(avgL)
-    
-f = open(os.path.join(csvpath, 'audiocsv.csv'), "a", newline = "")
+
+f = open(os.path.join(csvpath, 'audiocsv.csv'), "a", newline="")
 writer = csv.writer(f)
 writer.writerow(entrynum)
 writer.writerow(sounds)
@@ -107,5 +107,9 @@ f.close()
 
 data = pandas.read_csv(os.path.join(csvpath, 'audiocsv.csv'))
 data = data.transpose()
-data.rename(columns={0: 'AudioPath', 1: 'Channels', 2: 'Sample Rate (Hz)', 3: 'Number of Frames', 4: 'Time (sec)', 5:'Most Prominent Frequency (MPF)', 6: 'Loudness of MPF (dB)', 7: 'Time of Loudest Freq', 8: 'Average Loudness (dB)'}, inplace=True)
+data.rename(columns={0: 'AudioPath', 1: 'Channels',
+                     2: 'Sample Rate (Hz)', 3: 'Number of Frames',
+                     4: 'Time (sec)', 5: 'Most Prominent Frequency (MPF)',
+                     6: 'Loudness of MPF (dB)', 7: 'Time of Loudest Freq',
+                     8: 'Average Loudness (dB)'}, inplace=True)
 data.to_csv(os.path.join(csvpath, 'audiocsv.csv'), index=False)
