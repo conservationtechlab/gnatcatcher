8eef5b45a99d286383ad78152f135d7688d9a36d
anbreton <58612089+anbreton@users.noreply.github.com>
2021-06-22 19:20:09 -0700

    needs fix - only plays first guess as of now

--- checkBirdNET.py
+++ checkBirdNET.py
@@ -11,6 +11,7 @@ import numpy as np
 import wave
 import struct
 import pandas as pd
+import time
 
 audio = '/Users/amandabreton/Documents/GitHub/gnatcatcher/sounds/5D31ED38.WAV'
 data = '/Users/amandabreton/Documents/GitHub/gnatcatcher/BirdNet csv Files/5D31ED38.BirdNET.csv'
@@ -31,54 +32,50 @@ for i in range(1, len(df)):
         # knbirds.append(confid)
         bird = df[9][i]
         knbirds.append(bird)
-        start = df[4][i]
-        starts.append(start)
+        begin = df[4][i]
+        starts.append(begin)
         end = df[5][i]
         ends.append(end)
     else:
         pass
 
-# %%
-# setting up the sound playing
-
-lengths = []
-for i in range(len(starts)):
-    length = int(ends[i]) - int(starts[i])
-    lengths.append(length)
-
 # %%  hearing the sounds
 File = audio
-start = 10
-stop = 15
-length = stop - start
-chunk = 1024
 
-spf = wave.open(File, 'rb')
-signal = spf.readframes(-1)
-signal = np.fromstring(signal, 'Int16')
-p = pyaudio.PyAudio()
+for i in range(len(knbirds)):
+    print('Bird Guess is: ' + str(knbirds[i]))
+    print('The time is from ' + str(starts[i]) + ' sec to ' + str(ends[i]))
+    start = int(starts[i])
+    stop = int(ends[i])
+    length = int(ends[i]) - int(starts[i])
+    chunk = 1024
 
-stream = p.open(format=
-                p.get_format_from_width(spf.getsampwidth()),
-                channels=spf.getnchannels(),
-                rate=spf.getframerate(),
-                output=True)
+    spf = wave.open(File, 'rb')
+    signal = spf.readframes(-1)
+    # signal = np.fromstring(signal, 'Int16')
+    signal = np.frombuffer(signal, 'Int16')
+    p = pyaudio.PyAudio()
 
+    stream = p.open(format=
+                    p.get_format_from_width(spf.getsampwidth()),
+                    channels=spf.getnchannels(),
+                    rate=spf.getframerate(),
+                    output=True)
 
-pos = spf.getframerate()*length
-signal = signal[start*spf.getframerate():(start*spf.getframerate()) + pos]
-sig = signal[1:chunk]
+    pos = spf.getframerate()*length
+    signal = signal[start*spf.getframerate():(start*spf.getframerate()) + pos]
+    sig = signal[1:chunk]
 
-inc = 0
-data = 0
+    inc = 0
+    data = 0
 
-# play
-while data != '':
-    data = struct.pack("%dh" % (len(sig)), *list(sig))
-    stream.write(data)
-    inc = inc+chunk
-    sig = signal[inc:inc+chunk]
+    # play
+    while data != '':
+        data = struct.pack("%dh" % (len(sig)), *list(sig))
+        stream.write(data)
+        inc = inc+chunk
+        sig = signal[inc:inc+chunk]
 
-
-stream.close()
-p.terminate()
+    # stream.close()
+    # p.terminate()
+    time.sleep(2)
