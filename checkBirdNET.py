9bdd05563edcd931c679f978c7b27d4769afb0b7
anbreton <58612089+anbreton@users.noreply.github.com>
2021-06-22 20:30:39 -0700

    works need to add in parser

--- checkBirdNET.py
+++ checkBirdNET.py
@@ -6,21 +6,19 @@ Created on Tue Jun 22 16:19:38 2021
 @author: amandabreton
 """
 import pyaudio
-# import sys
 import numpy as np
 import wave
 import struct
 import pandas as pd
 import time
-
 audio = '/Users/amandabreton/Documents/GitHub/gnatcatcher/sounds/5D31ED38.WAV'
-data = '/Users/amandabreton/Documents/GitHub/gnatcatcher/BirdNet csv Files/5D31ED38.BirdNET.csv'
+datafile = '/Users/amandabreton/Documents/GitHub/gnatcatcher/BirdNet csv Files/5D31ED38.BirdNET.csv'
 threshold = 0.95
 # %%
 # setting up which sounds to hear
 
 df = pd.read_csv(
-    data, header=None)
+    datafile, header=None)
 
 knbirds = []
 starts= []
@@ -40,42 +38,39 @@ for i in range(1, len(df)):
         pass
 
 # %%  hearing the sounds
-File = audio
 
 for i in range(len(knbirds)):
+    File = audio
     print('Bird Guess is: ' + str(knbirds[i]))
-    print('The time is from ' + str(starts[i]) + ' sec to ' + str(ends[i]))
+    print('The time is from ' + str(starts[i]) + ' sec to ' + str(ends[i]) +
+          ' sec')
+# set desired values
     start = int(starts[i])
     stop = int(ends[i])
-    length = int(ends[i]) - int(starts[i])
-    chunk = 1024
+    length = stop - start
 
-    spf = wave.open(File, 'rb')
-    signal = spf.readframes(-1)
-    # signal = np.fromstring(signal, 'Int16')
-    signal = np.frombuffer(signal, 'Int16')
-    p = pyaudio.PyAudio()
+    # open wave file
+    wave_file = wave.open(audio, 'rb')
 
-    stream = p.open(format=
-                    p.get_format_from_width(spf.getsampwidth()),
-                    channels=spf.getnchannels(),
-                    rate=spf.getframerate(),
-                    output=True)
+    # initialize audio
+    py_audio = pyaudio.PyAudio()
+    stream = py_audio.open(format=py_audio.get_format_from_width(wave_file.getsampwidth()),
+                           channels=wave_file.getnchannels(),
+                           rate=wave_file.getframerate(),
+                           output=True)
 
-    pos = spf.getframerate()*length
-    signal = signal[start*spf.getframerate():(start*spf.getframerate()) + pos]
-    sig = signal[1:chunk]
+    # skip unwanted frames
+    n_frames = int(start * wave_file.getframerate())
+    wave_file.setpos(n_frames)
 
-    inc = 0
-    data = 0
+    # write desired frames to audio buffer
+    n_frames = int(length * wave_file.getframerate())
+    frames = wave_file.readframes(n_frames)
+    stream.write(frames)
 
-    # play
-    while data != '':
-        data = struct.pack("%dh" % (len(sig)), *list(sig))
-        stream.write(data)
-        inc = inc+chunk
-        sig = signal[inc:inc+chunk]
+    # close and terminate everything properly
+    stream.close()
+    py_audio.terminate()
+    wave_file.close()
 
-    # stream.close()
-    # p.terminate()
-    time.sleep(2)
+print('There were ' + str(len(knbirds)) + ' bird call guesses total')
