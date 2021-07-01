62fcdf38e87c300bec4c3cd55e3e047f6306fb96
anbreton <58612089+anbreton@users.noreply.github.com>
2021-07-01 01:04:33 -0700

    mircofaune to BirdNET, then most probable detections given
    uses a config file to parse in the paths and thresholds you want.

--- eventchecker.py
+++ eventchecker.py
@@ -7,23 +7,18 @@ Created on Thu Jun 24 19:25:10 2021
 """
 
 from microfaune.detection import RNNDetector
-import matplotlib.pyplot as plt
-import numpy as np
 import scipy.io.wavfile
-import contextlib
-import wave
 import os
 import pandas as pd
 import subprocess
 from subprocess import call
 import argparse
 import yaml
-# %%
-# path where the soudns are hosted
-# path where you want the csv file saved
+import numpy as np
+# %%  yaml file example: 
 # csvpath = "/Users/amandabreton/Documents/GitHub/gnatcatcher/"
 # path = '/Users/amandabreton/Documents/GitHub/gnatcatcher/sounds'
-# birdnetpath = '/Users/amandabreton/Documents/GitHub/BirdNET/analyze.py'
+# analyzepath = '/Users/amandabreton/Documents/GitHub/BirdNET/analyze.py'
 # threshold = 0.70
 
 # %% setup your files
@@ -35,10 +30,11 @@ with open(CONFIG_FILE) as f:
     configs = yaml.load(f, Loader=yaml.SafeLoader)
 csvpath = configs['csvpath']
 path = configs['path']
+analyzepath = configs['analyzepath']
 birdnetpath = configs['birdnetpath']
 threshold = configs['threshold']
 
-# %%
+# %% getting which files are audio
 if os.path.exists(os.path.join(csvpath, "eventlist.csv")):
     os.remove(os.path.join(csvpath, "eventlist.csv"))
 else:
@@ -62,8 +58,7 @@ for filename in os.listdir(path):
 sounds = [os.path.join(path, name) for name in os.listdir(path) if
           os.path.isfile(os.path.join(path, name))]
 
-# %%
-
+# %% using microfaune to filter audio files wtih events
 eventlist = []
 eventglobals = []
 goodlocals = []
@@ -80,18 +75,55 @@ for i in range(len(sounds)):
     else:
         pass
 
-# %%
+# %% putting the info into a data frame
 df = pd.DataFrame(list(zip(sounds, eventglobals, goodlocals)),
                   columns=['Audio_Paths', 'Highest_Event_Probability',
                            'Number_of_Events'])
 
-# %%
-os.chdir("/Users/amandabreton/Documents/GitHub/BirdNET")
+# %% sending files to BirdNET analyze
+os.chdir(birdnetpath)
 
 for i in range(len(df['Audio_Paths'])):
-    birdaud = str('python ') + str(birdnetpath) + str(' --i ') + str(df['Audio_Paths'][i])
+    birdaud = str('python ') + str(analyzepath) + str(' --i ') + str(df['Audio_Paths'][i])
     subprocess.call(birdaud, shell=True)
-    print('BirdNET done analyzing  audio file #' + str((df['Audio_Paths'][i])))
+    print('BirdNET done analyzing  audio file #' + str((i)))
 
 print('BirdNET done analyzing ' + str(len(df['Audio_Paths']))
       + ' audio files')
+
+# %% Most probable birds
+files = os.listdir(path)  # Get all the files in that directory
+txtfiles = []
+for filename in os.listdir(path):
+    if filename.endswith(".txt"):
+        name = os.path.join(path, filename)
+        txtfiles.append(name)
+    else:
+        nonimagecount = +1
+        continue
+# %%
+
+knbirds = []
+source = []
+confidences = []
+for k in range(len(txtfiles)):
+    birdtxt = txtfiles[k]
+    df = pd.read_csv(birdtxt, sep='\t')
+    for i in range(1, len(df)):
+        confid = df['Confidence'][i]
+        confid = float(confid)
+        if confid > threshold:
+            # knbirds.append(confid)
+            bird = df['Common Name'][i]
+            knbirds.append(bird)
+            audio = df['Begin File'][i]
+            source.append(audio)
+            confidences.append(confid)
+        else:
+            pass
+
+df = pd.DataFrame(list(zip(knbirds, confidences, source)),
+                  columns=['Species', 'Confidence', 'Audio Source'])
+
+print('The most probable bird detections in this batch are as follows:')
+print(df)
