342c11a34eb23bb80e4a385eaa52b9b0f481dfc7
anbreton <58612089+anbreton@users.noreply.github.com>
2021-07-09 13:07:24 -0700

    RIBBIT: spectrogram + pulse rate score graph
    shows the spectrogram of a given audio file 
    and shows the pulse rate score graph (ie the higher
    the score, the more likely that a repetitive sound is
    occurring in the audio
    
    next steps: 
    -add in parser
    -see how this is being done (ie is it 
    autocorrelation)

---
+++ ribbit_scoring.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  9 12:50:06 2021
+
+@author: amandabreton
+"""
+
+# suppress warnings
+import warnings
+warnings.simplefilter('ignore')
+# import packages
+import numpy as np
+from glob import glob
+import pandas as pd
+from matplotlib import pyplot as plt
+# local imports from opensoundscape
+from opensoundscape.audio import Audio
+from opensoundscape.spectrogram import Spectrogram
+from opensoundscape.ribbit import ribbit
+from opensoundscape.helpers import run_command
+
+# 55 set up the audio you want
+audio_path = np.sort(glob('./great_plains_toad_dataset/5D3C4530.WAV'))[0]
+
+# %%
+# create big visuals
+plt.rcParams['figure.figsize'] = [15, 8]
+
+# using our own data: just put the audio into a folder w/ same name
+# load the audio file into an OpenSoundscape Audio object
+audio = Audio.from_file(audio_path)
+
+# trim the audio to the time from 0-3 seconds for a closer look
+audio = audio.trim(0, 3)
+# create a Spectrogram object
+spectrogram = Spectrogram.from_audio(audio)
+spectrogram.plot()
+
+# minimum and maximum rate of pulsing (pulses per second) to search for
+pulse_rate_range = [10, 20]
+# look for a vocalization in the range of 1000-2000 Hz
+signal_band = [2000, 2500]
+# subtract the amplitude signal from these frequency ranges
+noise_bands = [[0, 200], [10000, 10100]]
+# divides the signal into segments this many seconds long
+# analyzes each independently
+window_length = 2  # (seconds)
+# if True, it will show the power spectrum plot for each audio segment
+show_plots = True
+
+# scoring
+spec = Spectrogram.from_audio(audio.from_file(audio_path))
+# run RIBBIT
+scores, times = ribbit(
+                    spec,
+                    pulse_rate_range=pulse_rate_range,
+                    signal_band=signal_band,
+                    window_len=window_length,
+                    noise_bands=noise_bands,
+                    plot=False)
+
+# plot the score vs time of each window
+plt.scatter(times, scores)
+plt.xlabel('window start time (sec)')
+plt.ylabel('RIBBIT score')
+plt.title('RIBBIT Scores')
+print('The max score is:')
+print(np.max(scores))
+print('at time ' + str(np.max(times)) + ' sec')
