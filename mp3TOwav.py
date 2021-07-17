67e691e7281fd4492d048fd86f22e528756b4fb5
anbreton <58612089+anbreton@users.noreply.github.com>
2021-07-16 17:35:51 -0700

    works for a file of mp3 files
    need to add in a parser if needed

--- mp3TOwav.py
+++ mp3TOwav.py
@@ -5,31 +5,31 @@ Created on Tue Jun  1 16:21:27 2021
 
 @author: amandabreton
 """
+#import utilities
 import os 
 #from os import path
 from pydub import AudioSegment
+from glob import glob
 
-#name = name of file you want to convert 
+# set up paths
+file_list = glob('/Users/amandabreton/Documents/GitHub/gnatcatcher/mp3s/*.mp3')
+path = '/Users/amandabreton/Documents/GitHub/gnatcatcher/mp3s'
+
+for i in range(len(file_list)):
+    name = file_list[i]
+    sound = AudioSegment.from_mp3(name)
+    sound.export(name, format= "wav")
+
+
+# %%
+#name = name of file you want to convert
 name = 'XC564909 - Cactus Wren - Campylorhynchus brunneicapillus.mp3'
 # dst = name you want for the .wav file 
 dst = "cactuswren1.wav"
 
-path = '/Users/amandabreton/Documents/GitHub/gnatcatcher/mp3s'
+
 src  = os.path.join(path, name)
 
 # convert wav to mp3                                                            
 sound = AudioSegment.from_mp3(src)
 sound.export(dst, format="wav")
-
-# got this error: 
-#FileNotFoundError: [Errno 2] No such file or directory: 'ffprobe'
-# to fix: in terminal 
-#/usr/bin/ruby -e "$(curl -fsSL
-#https://raw.githubusercontent.com/Homebrew/install/master/install)"  
-# basically needed to install homebrew: 
-# brew install ffmpeg
-# this looked kind of suspicious not gonna lie 
-# and took like 20 minutes to install 
-# didn't work 
-
-    
