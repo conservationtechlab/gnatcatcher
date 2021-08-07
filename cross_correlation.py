a5503a87da19450c92531c7ccd25c23c6c6b9cbe
anbreton <58612089+anbreton@users.noreply.github.com>
2021-08-06 22:11:18 -0700

    Performs 1D cross correlation on a set of files.
    Compares it to master set of files to try to determine a match.

--- cross_correlation.py
+++ cross_correlation.py
@@ -4,6 +4,9 @@
 Created on Tue Jul 27 23:09:06 2021
 
 @author: amandabreton
+
+Performs 1D cross correlation on a set of files.
+Compares it to master set of files to try to determine a match.
 """
 # import modules
 from scipy import signal
@@ -19,22 +22,18 @@ import yaml
 import pandas as pd
 import os
 # %% set up paths to mastlist of audio and unknown sample
-# parser = argparse.ArgumentParser()
-# parser.add_argument('config_filename')
-# args = parser.parse_args()
-# CONFIG_FILE = args.config_filename
-# with open(CONFIG_FILE) as f:
-#     configs = yaml.load(f, Loader=yaml.SafeLoader)
-# ccpath = configs['ccpath']
-# unknbird = configs['unknbird']
-
-# %%
-ccpath = '/Users/amandabreton/Documents/GitHub/gnatcatcher/cross_correlation_audio'
-unknbirds = '/Users/amandabreton/Documents/GitHub/gnatcatcher/cross_correlation_audio/UnknownBirds'
+parser = argparse.ArgumentParser()
+parser.add_argument('config_filename')
+args = parser.parse_args()
+CONFIG_FILE = args.config_filename
+with open(CONFIG_FILE) as f:
+    configs = yaml.load(f, Loader=yaml.SafeLoader)
+masterlistpath = configs['masterlistpath']
+unknbirds = configs['unknbirds']
 
 # %%  for mac os remove .DS_Store files
-if os.path.exists(os.path.join(ccpath, ".DS_Store")):
-    os.remove(os.path.join(ccpath, ".DS_Store"))
+if os.path.exists(os.path.join(masterlistpath, ".DS_Store")):
+    os.remove(os.path.join(masterlistpath, ".DS_Store"))
 else:
     # print("no .DS_Store files")
     pass
@@ -45,25 +44,12 @@ else:
     # print("no .DS_Store files")
     pass
 # %% get list of paths to masterlist files and list of names in the masterlist
-file_paths = glob(str(ccpath)+'/*.wav')
-filenames = next(walk(ccpath), (None, None, []))[2]  # [] if no file
+file_paths = glob(str(masterlistpath)+'/*.wav')
+filenames = next(walk(masterlistpath), (None, None, []))[2]  # [] if no file
 
 # %%
 unbirds_paths = glob(str(unknbirds)+'/*.wav')
 unbirdsnames = next(walk(unknbirds), (None, None, []))[2]  # [] if no file
-
-# %% for macOS, remove .DS_Store file
-if os.path.exists(os.path.join(ccpath, ".DS_Store")):
-    os.remove(os.path.join(ccpath, ".DS_Store"))
-else:
-    # print("no .DS_Store files")
-    pass
-
-# %% set up dataframe for later
-times = np.arange(0,60,10)
-#df = pd.DataFrame(index=unbirdsnames, columns=times)
-df_corr = pd.DataFrame()
-df_matches = pd.DataFrame()
 
 # %%
 matches = []
@@ -78,8 +64,10 @@ for k in range(len(unbirds_paths)):
     for i in range(len(file_paths)):
         knbird = file_paths[i]
         knbird_sr, knbird_samples = wavfile.read(knbird)
-        frequencies, times, knspectrogram = signal.spectrogram(knbird_samples, knbird_sr)
-        #corr = signal.correlate2d(segment, knspectrogram, boundary='symm', mode='same')
+        frequencies, times,
+        knspectrogram = signal.spectrogram(knbird_samples, knbird_sr)
+        # corr = signal.correlate2d(spectrogram, knspectrogram, boundary='symm', mode='same')
+        #corr = signal.correlate2d(spectrogram, knspectrogram)
         corr = signal.correlate(spectrogram, knspectrogram)
         maxcorr = np.max(corr)
         maxcorrs.append(maxcorr)
@@ -94,4 +82,4 @@ for k in range(len(unbirds_paths)):
 df = pd.DataFrame(list(zip(unbirdsnames, matches, confidences)),
                   columns=['File Name', 'Closest Matched Species',
                            'Confidence Compared to Autocorrelation'])
-df.to_csv(r'/Users/amandabreton/Desktop/testcc.csv')
+df.to_csv(r'/gnatcatcher_data/gnatcatcher/cross_correlation_audio/crosscorrelation.csv')
